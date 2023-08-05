14a3075292780d06099e689dece40cc5d90815d0
pandupan <pangestup60@gmail.com>
2023-08-05 15:27:29 +0700

    [ADD] - Header Done

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -5,25 +5,23 @@ import { FaLinkedinIn } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <div className='flex w-full justify-around bg-colorfull-darkblue h-[60px] '>
-      <div>
-        <div className='flex items-center justify-center text-white'>
-          <MdEmail size={25}/>
+    <div className='hidden lg:flex w-full justify-between bg-colorfull-darkblue h-[60px] text-xs'>
+      <div className='flex justify-center items-center gap-8 ml-16'>
+        <div className='flex items-center justify-center text-white gap-2'>
+          <MdEmail size={15}/>
           <p>emciartalents@gmail.com</p>
         </div>
-        <div className='flex items-center justify-center text-white'>
-          <BsFillClockFill size={25}/>
+        <div className='flex items-center justify-center text-white gap-2'>
+          <BsFillClockFill size={15}/>
           <p>Mon - Fri: 9.00 am - 6.00 pm</p>
         </div>
       </div>
-      <div>
+      <div className='flex justify-center items-center gap-4 mr-16'>
         <div className='flex items-center justify-center text-white'>
           <p>Emciar is innately talented at connecting talents and clients.</p>
-          <FaLinkedinIn/>
         </div>
-
-      </div>
-      
+          <FaLinkedinIn className="flex justify-center items-center text-white" size={15}/>
+      </div> 
     </div>
   )
 }

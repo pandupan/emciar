41026183041e88bf506b287c4bcd9b312a1a6a00
EgaAprianto8 <egaaprianto8@gmail.com>
2023-08-06 08:25:55 +0700

    why

--- components/pages/landing-page/Hiring.tsx
+++ components/pages/landing-page/Hiring.tsx
@@ -5,14 +5,14 @@ const Hiring = () => {
   return (
     <div className="container mx-auto">
       <div className="">
-        <p className="text-[#7141b1] sm:px-6 md:px-32 lg:px-24 px-0 md:text-sm sm:text-xs text-xs font-extrabold">
-            HIRING MANDATES
+        <p className="text-[#7141b1] sm:px-6 lg:px-24 px-0 md:text-sm sm:text-xs text-xs font-extrabold">
+          HIRING MANDATES
         </p>
-        <p className="md:text-4xl sm:text-3xl text-3xl mt-2 font-black sm:px-6 md:px-32 lg:px-24 px-0 sm:w-full lg:w-full md:w-[700px]">
+        <p className="md:text-4xl sm:text-3xl text-3xl mt-2 font-black sm:px-6  lg:px-24 px-0 sm:w-full lg:w-full md:w-[500px]">
           TYPICAL TYPES WE WORK MOST OFTEN
         </p>
         <div className="flex flex-col sm:flex-row justify-center mt-[65px] flex-wrap">
-          <div className="flex flex-col items-center  md:w-[350px] w-full">
+          <div className="flex flex-col items-center  md:w-[350px] w-full lg:m-6">
             <Image
               src="/images/WORLDWIDE.png"
               width={45}
@@ -20,14 +20,12 @@ const Hiring = () => {
               alt="WORLDWIDE"
               className="sm:mt-4 mt-6"
             />
-            <p className="sm:text-lg text-xl font-extrabold mt-6">
-                WORLDWIDE
-            </p>
+            <p className="sm:text-lg text-xl font-extrabold mt-6">WORLDWIDE</p>
             <p className="text-[#6d6d6d] mt-2 text-center sm:w-full w-[150px]">
-                Locating talent in 180+ countries
+              Locating talent in 180+ countries
             </p>
           </div>
-          <div className="flex flex-col items-center  sm:w-[350px] w-full sm:mt-0 mt-16">
+          <div className="flex flex-col items-center  sm:w-[350px] w-full sm:mt-0 mt-16 lg:m-6">
             <Image
               src="/images/HIRING-EXECUTIVES.png"
               width={50}
@@ -36,13 +34,13 @@ const Hiring = () => {
               className="mt-4"
             />
             <p className="sm:text-lg text-xl font-extrabold mt-6 text-center sm:w-full w-[150px]">
-                HIRING EXECUTIVES
+              HIRING EXECUTIVES
             </p>
             <p className="text-[#6d6d6d] mt-2 text-center sm:w-full w-[150px]">
-                C-suite and Leadership hiring
+              C-suite and Leadership hiring
             </p>
           </div>
-          <div className="sm:hidden md:hidden lg:flex flex flex-col items-center sm:w-[350px] w-full sm:mt-0 mt-16">
+          <div className="sm:hidden md:hidden lg:hidden xl:flex flex flex-col items-center sm:w-[350px] w-full sm:mt-0 mt-16 lg:m-6">
             <Image
               src="/images/TECHNICAL.png"
               width={50}
@@ -51,13 +49,13 @@ const Hiring = () => {
               className="mt-4"
             />
             <p className="sm:text-lg text-xl font-extrabold mt-6 text-center ">
-                TECHNICAL
+              TECHNICAL
             </p>
             <p className="text-[#6d6d6d] mt-2 text-center sm:w-full w-[150px]">
-                Web, IoT, Software and Hardware
+              Web, IoT, Software and Hardware
             </p>
           </div>
-          <div className="hidden sm:flex flex-col items-center sm:w-[350px] w-full md:mt-0 mt-16">
+          <div className="hidden sm:flex flex-col items-center sm:w-[350px] w-full md:mt-0 mt-16 lg:m-6">
             <Image
               src="/images/MASS-HIRING.png"
               width={45}
@@ -66,13 +64,13 @@ const Hiring = () => {
               className="mt-4"
             />
             <p className="sm:text-lg text-xl font-extrabold mt-6 text-center">
-                MASS HIRING
+              MASS HIRING
             </p>
             <p className="text-[#6d6d6d] mt-2 text-center sm:w-full w-[150px]">
-                10s to 100s new hires per month
+              10s to 100s new hires per month
             </p>
           </div>
-          <div className="hidden sm:flex flex-col items-center sm:w-[350px] w-full sm:mt-0 mt-16">
+          <div className="hidden sm:flex flex-col items-center sm:w-[350px] w-full sm:mt-0 mt-16 lg:m-6">
             <Image
               src="/images/OFFICE.png"
               width={50}
@@ -80,11 +78,9 @@ const Hiring = () => {
               alt="OFFICE"
               className="mt-4"
             />
-            <p className="sm:text-lg text-xl font-extrabold mt-6">
-                OFFICE
-            </p>
+            <p className="sm:text-lg text-xl font-extrabold mt-6">OFFICE</p>
             <p className="text-[#6d6d6d] mt-2 sm:w-full w-[150px] text-center">
-                Reception, Support and Facilities Staff
+              Reception, Support and Facilities Staff
             </p>
           </div>
         </div>
---
+++ components/pages/landing-page/Why.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { FaSearchPlus } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa";
+import { FaNetworkWired } from "react-icons/fa";
+import { FaHandshake } from "react-icons/fa";
+
+const Why = () => {
+  return (
+    <div className="container mx-auto mt-64 md:mt-28 mb-12">
+      <div>
+        <p className="font-black sm:text-4xl text-2xl text-center sm:m-16 mb-28 ">Why Emciar?</p>
+      </div>
+      <div className="flex flex-col sm:flex-row sm:items-start items-center md:gap-20 lg:gap-24 xl:gap-24 justify-center flex-wrap">
+        <div className="flex flex-col  items-center md:w-[90px] lg:w-[120px] xl:w-[180px] w-[200px]">
+          <FaSearchPlus size={55} />
+          <p className="text-center font-extrabold text-xl my-4">
+            Better Finders
+          </p>
+          <p className="text-[#6d6d6d] text-center">
+            Emciar clients receive far more accurate profiles at even better
+            speed and turnaround time as compared to our industry peers across
+            India and worldwide.
+          </p>
+        </div>
+        <div className="flex flex-col items-center md:w-[90px] lg:w-[120px] xl:w-[180px] w-[200px] sm:mt-0 mt-28">
+          <FaRegEye size={55} />
+          <p className="text-center font-extrabold text-xl my-4">
+            Transparent
+          </p>
+          <p className="text-[#6d6d6d] text-center">
+            Our clients have complete visibility over our entire operations and
+            delivery process with dedicated single point of contacts to ensure
+            they are always up to date and speed with latest progress on their
+            mandates.
+          </p>
+        </div>
+        <div className="flex flex-col items-center md:w-[90px] lg:w-[120px] xl:w-[180px] w-[200px] sm:mt-0 mt-28">
+          <FaNetworkWired size={55} />
+          <p className="text-center font-extrabold text-xl my-4">
+            Well Networked
+          </p>
+          <p className="text-[#6d6d6d] text-center">
+            Most of the talent that we find for our clients are from our
+            existing network itself courtesy our team's several decades of
+            experience and professional journeys.
+          </p>
+        </div>
+        <div className="flex flex-col items-center md:w-[90px] lg:w-[120px] xl:w-[180px] w-[200px] sm:mt-0 mt-28">
+          <FaHandshake size={55} />
+          <p className="text-center font-extrabold text-xl my-4">
+            More Trusted
+          </p>
+          <p className="text-[#6d6d6d] text-center">
+            Our network includes talent with a minimum of a few years to several
+            decades of experience across sectors and geographies.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Why;

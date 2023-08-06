2bea23ab7d207dbfe371bb022e61595d7ea049b9
pandupan <pangestup60@gmail.com>
2023-08-06 08:37:39 +0700

    [ADD] - Hero Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
 import Hiring from '../components/pages/landing-page/Hiring'
 import Why from '@/components/pages/landing-page/Why'
+import Hero from '@/components/pages/landing-page/Hero'
 export default function Home() {
   return (
     <>
-      Hero Section
+      <Hero/>
       <Hiring/>
       <Why/>
     </>

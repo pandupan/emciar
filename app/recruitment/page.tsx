3626f076c6d9391ade38f3d1e02b17422da92f0d
EgaAprianto8 <egaaprianto8@gmail.com>
2023-08-07 14:47:20 +0700

    HiringFormats

--- app/recruitment/page.tsx
+++ app/recruitment/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Recruitment from '@/components/pages/recruitment-page/Recruitment'
+import HiringFormats from '@/components/pages/recruitment-page/HiringFormats'
 
 const RecruitmentPage = () => {
   return (
     <div>
       <Recruitment/>
+      <HiringFormats/>
     </div>
   )
 }

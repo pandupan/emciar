b98af88b0b13e12f8decac9134a734b502be244a
EgaAprianto8 <egaaprianto8@gmail.com>
2023-08-09 15:21:24 +0700

    [UPDATE] Counseling Stage

--- components/pages/counseling-page/stages-counseling.tsx
+++ components/pages/counseling-page/stages-counseling.tsx
@@ -7,7 +7,7 @@ const StagesCounseling = () => {
         Job Counseling Stages
       </h1>
       <div className="flex flex-col sm:flex-row gap-8 flex-wrap sm:px-0 px-8 justify-center">
-      <div className="flex flex-col sm:w-[355px] sm:h-[250px] h-[300px] bg-[#262051] py-12 px-8 border border-[#46416b]">
+      <div className="relative overflow-hidden flex flex-col sm:w-[355px] sm:h-[250px] h-[300px] bg-[#262051] py-12 px-8 border border-[#46416b]">
         <h1 className="text-white text-xl font-extrabold">
           CV Scan
         </h1>
@@ -15,8 +15,13 @@ const StagesCounseling = () => {
           Scrutinizing your resume critically from
           eyes of HR
         </p>
+        <div className="absolute bg-[#6ec1e4] rounded-full -right-24 -bottom-24 w-[200px] h-[200px]">
+        </div>
+        <div className='absolute justify-center flex items-center bottom-6 right-6 border-[2px] text-black font-black text-xl border-black rounded-full p-4 w-4 h-4'>
+          1
+        </div>
       </div>
-      <div className="flex flex-col sm:w-[355px] sm:h-[250px] h-[300px] bg-[#262051] py-12 px-8 border border-[#46416b]">
+      <div className="relative overflow-hidden flex flex-col sm:w-[355px] sm:h-[250px] h-[300px] bg-[#262051] py-12 px-8 border border-[#46416b]">
         <h1 className="text-white text-xl font-extrabold">
           Professional Advisory
         </h1>
@@ -24,8 +29,13 @@ const StagesCounseling = () => {
           Aligning your career goals and
           streamlining directions
         </p>
+        <div className="absolute bg-[#6ec1e4] rounded-full -right-24 -bottom-24 w-[200px] h-[200px]">
+        </div>
+        <div className='absolute justify-center flex items-center bottom-6 right-6 border-[2px] text-black font-black text-xl border-black rounded-full p-4 w-4 h-4'>
+          2
+        </div>
       </div>
-      <div className="flex flex-col sm:w-[355px] sm:h-[250px] h-[300px] bg-[#262051] py-12 px-8 border border-[#46416b]">
+      <div className="relative overflow-hidden flex flex-col sm:w-[355px] sm:h-[250px] h-[300px] bg-[#262051] py-12 px-8 border border-[#46416b]">
         <h1 className="text-white text-xl font-extrabold">
           Job Scouting
         </h1>
@@ -33,22 +43,37 @@ const StagesCounseling = () => {
           You representative to find and apply
           jobs on your behalf
         </p>
+        <div className="absolute bg-[#6ec1e4] rounded-full -right-24 -bottom-24 w-[200px] h-[200px]">
+        </div>
+        <div className='absolute justify-center flex items-center bottom-6 right-6 border-[2px] text-black font-black text-xl border-black rounded-full p-4 w-4 h-4'>
+          3
+        </div>
       </div>
-      <div className="flex flex-col sm:w-[355px] xl:w-[520px] xl:h-[225px] sm:h-[250px] h-[300px] bg-[#262051] py-12 px-8 border border-[#46416b]">
+      <div className="relative overflow-hidden flex flex-col sm:w-[355px] xl:w-[520px] xl:h-[225px] sm:h-[250px] h-[300px] bg-[#262051] py-12 px-8 border border-[#46416b]">
         <h1 className="text-white text-xl font-extrabold">
           Interview Support
         </h1>
         <p className="text-white mt-4">
         Detailed evaluation of your employment contracts
         </p>
+        <div className="absolute bg-[#6ec1e4] rounded-full -right-24 -bottom-24 w-[200px] h-[200px]">
+        </div>
+        <div className='absolute justify-center flex items-center bottom-6 right-6 border-[2px] text-black font-black text-xl border-black rounded-full p-4 w-4 h-4'>
+          4
+        </div>
       </div>
-      <div className="flex flex-col sm:w-[355px] xl:w-[520px] xl:h-[225px] sm:h-[250px] h-[300px] bg-[#262051] py-12 px-8 border border-[#46416b]">
+      <div className="relative overflow-hidden flex flex-col sm:w-[355px] xl:w-[520px] xl:h-[225px] sm:h-[250px] h-[300px] bg-[#262051] py-12 px-8 border border-[#46416b]">
         <h1 className="text-white text-xl font-extrabold">
           Joining Support
         </h1>
         <p className="text-white mt-4">
         Smooth landing ensurance for easy probation completion
         </p>
+        <div className="absolute bg-[#6ec1e4] rounded-full -right-24 -bottom-24 w-[200px] h-[200px]">
+        </div>
+        <div className='absolute justify-center flex items-center bottom-6 right-6 border-[2px] text-black font-black text-xl border-black rounded-full p-4 w-4 h-4'>
+          5
+        </div>
       </div>
       </div>
     </div>

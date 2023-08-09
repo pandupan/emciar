ab009c4fd3658480159a885f4ad4c71b1f6d8dae
EgaAprianto8 <egaaprianto8@gmail.com>
2023-08-09 16:46:08 +0700

    [FIXING] HERO CONTACTS

--- components/pages/contacts-page/hero-contatcs.tsx
+++ components/pages/contacts-page/hero-contatcs.tsx
@@ -5,12 +5,12 @@ import {SlArrowRight} from "react-icons/sl"
 const HeroContacts = () => {
   return (
     <>
-    <div className="relative flex justify-center bg-[#262051] items-center w-full h-[50vh] mt-0 sm:mt-2 md:mt-0 xl:mt-[140px] mb-28 z-1">
-      <div className="container flex flex-row justify-between p-32">
-          <h1 className="text-5xl text-white font-black">
+    <div className="relative flex justify-center bg-[#262051] items-center w-full sm:h-[50vh] h-[300px] mt-0 sm:mt-2 md:mt-0 xl:mt-[140px] mb-28 z-1">
+      <div className="container flex sm:flex-col flex-col lg:flex-row justify-between sm:p-20 xl:32">
+          <h1 className="sm:text-5xl text-4xl text-white font-black">
             Contacts
           </h1>
-          <div className="flex flex-row items-center">
+          <div className="flex flex-row mt-2 items-center">
             <Link href="/">
               <h2 className="text-[#aeaacb] font-black text-sm hover:text-white">
                 HOME
